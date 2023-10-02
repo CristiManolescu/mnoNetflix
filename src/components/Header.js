2e5edfde4b3427227a1ed4ae1656550d12694e13
CristiManolescu <m_cristian_s96@yahoo.com>
2023-10-02 14:18:50 +0300

    Add: Sign In/Up form & Login/header components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img src="/logo.png" alt="logo" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Login.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/6e7c7e50-8c62-42e8-81e5-41b2d7740d67/RO-en-20230925-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="background"
+        />
+      </div>
+      <form className="absolute p-12 bg-black w-3/12 mx-auto my-36 right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-2">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now!"
+            : "Already registered? Sign In now!"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
