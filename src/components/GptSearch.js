7327a0d660ae947f84cdd27eea1f8325a858f9ed
CristiManolescu <m_cristian_s96@yahoo.com>
2023-10-09 15:51:19 +0300

    Created the components for GptSearch

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,5 +1,17 @@
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import GptSearchBar from "./GptSearchBar";
+import { BACKGROUND } from "../utils/constants";
+
 const GptSearch = () => {
-  return <div>GptSearch</div>;
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img src={BACKGROUND} alt="background" />
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggestions />
+    </div>
+  );
 };
 
 export default GptSearch;
