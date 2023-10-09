31037e36d9f6d49adffe657090a6804975e8f5a6
CristiManolescu <m_cristian_s96@yahoo.com>
2023-10-09 13:48:47 +0300

    Toggle for GPT Search / Browse Page

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -5,17 +5,28 @@ import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTopRatedMovies from "../hooks/useTopRatedMovies";
 import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import GptSearch from "./GptSearch";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
   useNowPlayingMovies();
   usePopularMovies();
   useTopRatedMovies();
   useUpcomingMovies();
+
+  const gptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   return (
     <div>
       <Header />
-      <MainContainer />
-      <SecondaryContainer />
+      {gptSearch ? (
+        <GptSearch />
+      ) : (
+        <>
+          <MainContainer />
+          <SecondaryContainer />
+        </>
+      )}
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux/es/hooks/useSelector";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -42,11 +43,21 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img src="/logo.png" alt="logo" />
+      <img className="w-44" src="/logo.png" alt="logo" />
       {user && (
         <div className="flex p-2">
+          <button
+            className="bg-purple-800 text-white rounded-lg py-2 px-4 mx-4 my-2 hover:opacity-80"
+            onClick={handleGptSearchClick}
+          >
+            GPT Search
+          </button>
           <img className="w-12 h-12" src={user?.photoURL} alt="user-icon" />
           <button onClick={handleSignOut} className="font-bold text-white">
             (Sign Out)
